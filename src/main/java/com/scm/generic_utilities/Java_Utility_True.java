95546765b5dcc0dbec9c39995057681babd38301
Sami <samishaikh77@gmail.com>
2026-04-06 22:36:37 +0530

    created java utility and webdriver utility

---
+++ src/main/java/com/scm/generic_utilities/Java_Utility_True.java
@@ -0,0 +1,20 @@
+package com.scm.generic_utilities;
+
+import java.util.Date;
+import java.util.Random;
+
+public class Java_Utility_True {
+
+	// Generate Random Number
+	public int getRandomNumber() {
+		Random random = new Random();
+		int randomInt = random.nextInt(1000);
+		return randomInt;
+	}
+
+	// Generate System date
+	public String getSystemDate() {
+		Date date = new Date();
+		return date.toString();
+	}
+}
