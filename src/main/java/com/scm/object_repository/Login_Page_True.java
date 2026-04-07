ea2094ccb98f89a5d3fb344e9c0cd1ec87fb81a8
Sami <samishaikh77@gmail.com>
2026-04-07 22:55:51 +0530

    created object_repo, Login_Page, SuperBaseClass,ObjectUtilityClass

---
+++ src/main/java/com/scm/object_repository/Login_Page_True.java
@@ -0,0 +1,89 @@
+package com.scm.object_repository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class Login_Page_True {
+
+	WebDriver driver;
+
+	public Login_Page_True(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id = "login:username")
+	private WebElement userNameField;
+	
+	@FindBy(id = "login:password")
+	private WebElement passwordField;
+	
+	@FindBy(id = "login:type")
+	private WebElement loginType;
+	
+	@FindBy(xpath = "//option[text()='Retailer']")
+	private WebElement retailer;
+	
+	@FindBy(xpath = "//option[text()='Manufacturer']")
+	private WebElement manufacturer;
+	
+	@FindBy(xpath = "//option[text()='Admin']")
+	private WebElement admin;
+	
+	@FindBy(xpath = "//input[@type='submit']")
+	private WebElement loginBtn;
+	
+	@FindBy(xpath = "//span[text()=' * Username or Password is incorrect. ']")
+	private WebElement errorMsg;
+
+	public WebElement getErrorMsg() {
+		return errorMsg;
+	}
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public WebElement getUserNameField() {
+		return userNameField;
+	}
+
+	public WebElement getPasswordField() {
+		return passwordField;
+	}
+
+	public WebElement getLoginType() {
+		return loginType;
+	}
+
+	public WebElement getRetailer() {
+		return retailer;
+	}
+
+	public WebElement getManufacturer() {
+		return manufacturer;
+	}
+
+	public WebElement getAdmin() {
+		return admin;
+	}
+
+	public WebElement getLoginBtn() {
+		return loginBtn;
+	}
+	
+	// Business Logic
+	
+	public void loginAsAdmin() {
+		getUserNameField().sendKeys("Admin");
+		getPasswordField().sendKeys("Admin123");
+		Select sel = new Select(loginType);
+		sel.selectByVisibleText("//option[text()='Admin']");
+		getLoginBtn().click();
+		
+	}
+
+}
